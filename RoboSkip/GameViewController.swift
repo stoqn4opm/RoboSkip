7f06694cb632a40999f2fac2dc98920673cfd228
Стоян Стоянов <stoqn.stoqnov.93@gmail.com>
2017-01-27 10:49:12 +0200

    removed unused code

--- RoboSkip/GameViewController.swift
+++ RoboSkip/GameViewController.swift
@@ -14,22 +14,8 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.view = GameManager.shared.skView
         GameManager.shared.loadMenuScene()
-        //
-//        if let view = self.view as! SKView? {
-//            // Load the SKScene from 'GameScene.sks'
-//                // Set the scale mode to scale to fit the window
-//                scene.scaleMode = .aspectFill
-//            
-//                // Present the scene
-//                view.presentScene(scene)
-//
-//            
-//            view.showsFPS = true
-//            view.showsNodeCount = true
-//        }
     }
 
     override var prefersStatusBarHidden: Bool {
