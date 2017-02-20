15a915d33f478ada4e4bdc9edfe08ff61fde2e04
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2017-02-20 23:39:57 +0200

    some minor tweaks for marking where the force field is applied

--- RoboSkip/HelperExtensions.swift
+++ RoboSkip/HelperExtensions.swift
@@ -33,29 +33,29 @@ extension Loggable {
         }
     }
 }
-//
-//extension CGPoint {
-//    static var normalizedMiddle: CGPoint {
-//        return CGPoint(x: 0.5, y: 0.5)
-//    }
-//    
-//    static var normalizedUpperLeft: CGPoint {
-//        return CGPoint(x: 0, y: 1)
-//    }
-//    
-//    static var normalizedUpperRight: CGPoint {
-//        return CGPoint(x: 1, y: 1)
-//    }
-//    
-//    static var normalizedUpperCenter: CGPoint {
-//        return CGPoint(x: 0.5, y: 1)
-//    }
-//    
-//    static var normalizedLowerCenter: CGPoint {
-//        return CGPoint(x: 0.5, y: 0)
-//    }
-//    
-//    static var normalizedLowerLeft: CGPoint {
-//        return CGPoint(x: 0, y: 0)
-//    }
-//}
+
+extension CGPoint {
+    static var normalizedMiddle: CGPoint {
+        return CGPoint(x: 0.5, y: 0.5)
+    }
+    
+    static var normalizedUpperLeft: CGPoint {
+        return CGPoint(x: 0, y: 1)
+    }
+    
+    static var normalizedUpperRight: CGPoint {
+        return CGPoint(x: 1, y: 1)
+    }
+    
+    static var normalizedUpperCenter: CGPoint {
+        return CGPoint(x: 0.5, y: 1)
+    }
+    
+    static var normalizedLowerCenter: CGPoint {
+        return CGPoint(x: 0.5, y: 0)
+    }
+    
+    static var normalizedLowerLeft: CGPoint {
+        return CGPoint(x: 0, y: 0)
+    }
+}
--- RoboSkip/LevelScene.swift
+++ RoboSkip/LevelScene.swift
@@ -17,8 +17,6 @@ class LevelScene: SKScene {
     
     var character: CharacterNode!
     var radiaGravity: SKFieldNode?
-    var tapHoldRecognizer: UIGestureRecognizer?
-    
     
     //MARK: - Entrance Point Of Scene
     override func didMove(to view: SKView) {
@@ -56,21 +54,53 @@ extension LevelScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         radiaGravity?.isEnabled = true
         radiaGravity?.position = touches.first!.location(in: self)
+        markTouch(at: touches.first!.location(in: self))
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         radiaGravity?.position = touches.first!.location(in: self)
+        let mark = childNode(withName: "touchMarker")
+        mark?.position = touches.first!.location(in: self)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         radiaGravity?.isEnabled = false
+        removeMark()
     }
-}
-
-//MARK: - Gesture Recognizer Delegate Methods
-extension LevelScene: UIGestureRecognizerDelegate {
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
-                           shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
+    
+    func markTouch(at point: CGPoint) {
+        let mark = SKSpriteNode(imageNamed: "fieldChange")
+        mark.name = "touchMarker"
+        mark.alpha = 0
+        mark.zPosition = -1
+        mark.anchorPoint = .normalizedMiddle
+        mark.position = point
+        mark.setScale(0.1)
+        self.addChild(mark)
+        mark.run(markAction)
+    }
+    
+    func removeMark() {
+        for child in children {
+            if child.name == "touchMarker" {
+                child.removeAllActions()
+                child.run(unMarkAction)
+            }
+        }
+    }
+    
+    var markAction: SKAction {
+        let fadeIn = SKAction.fadeIn(withDuration: 0.4)
+        let scaleIn = SKAction.scale(to: 0.8, duration: 0.4)
+        return SKAction.group([fadeIn, scaleIn])
+    }
+    
+    var unMarkAction: SKAction {
+        let fadeOut = SKAction.fadeOut(withDuration: 0.1)
+        let scaleDown = SKAction.scale(to: 0.1, duration: 0.1)
+        SKAction.removeFromParent()
+        let group = SKAction.group([fadeOut, scaleDown])
+        group.timingMode = .easeIn
+        return SKAction.sequence([group, SKAction.removeFromParent()])
     }
 }
